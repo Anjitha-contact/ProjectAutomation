0ea2634e976846258d7410e9e2938d29af548023
Abhijeeth <Anjitha-contact@gmail.com>
2025-09-21 12:05:51 +0530

    commiton2109

--- src/main/java/pages/AdminUsersPage.java
+++ src/main/java/pages/AdminUsersPage.java
@@ -1,21 +1,26 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utilities.PageUtility;
 
 public class AdminUsersPage {
 	public WebDriver driver;
+	private WebDriverWait wait;
 	
 public AdminUsersPage(WebDriver driver)
 {
 	this.driver=driver;
 	PageFactory.initElements(driver, this);	
+	wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 }
 
 @FindBy(xpath="//a[@href='https://groceryapp.uniqassosiates.com/admin/list-admin' and @class='small-box-footer']")private WebElement adminuserbutton;
@@ -33,6 +38,7 @@ public AdminUsersPage(WebDriver driver)
 
 public AdminUsersPage clickonadminuser()
 {
+
 	adminuserbutton.click();
 	return this;
 }
